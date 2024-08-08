ff06d4f46b7a7dad03b7ec1cfc1fefd592459bc2
AlanZheng <a0955857152@gmail.com>
2024-08-08 09:23:04 +0800

    sudo cp /etc/ssh/ssh_host_rsa_key.ori /etc/ssh/ssh_host_rsa_key sudo chmod 600 /etc/ssh/ssh_host_rsa_key ...

--- rsyncd/docker-entrypoint.sh
+++ rsyncd/docker-entrypoint.sh
@@ -8,5 +8,14 @@ else
 fi
 
 cat /home/rsyncd/rsyncd.secrets | sudo chpasswd
+
+sudo cp /home/rsyncd/.ssh/authorized_keys.ori /home/rsyncd/.ssh/authorized_keys
 sudo chown rsyncd:rsyncd /home/rsyncd/.ssh/authorized_keys
+
+sudo cp /etc/ssh/ssh_host_rsa_key.ori /etc/ssh/ssh_host_rsa_key 
+sudo chmod 600 /etc/ssh/ssh_host_rsa_key
+
+sudo cp /etc/ssh/ssh_host_rsa_key.pub.ori /etc/ssh/ssh_host_rsa_key.pub 
+sudo chmod 644 /etc/ssh/ssh_host_rsa_key.pub
+
 sudo /usr/sbin/sshd -D
